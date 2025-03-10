ff068871e00984aed69687cda991989d5df0745c
Minh Vuong Duong <minhvuogdzai@MacBook-Pro-cua-Minh.local>
2025-03-10 10:05:38 +0700

    Initial commit

---
+++ javascript/main.js
@@ -0,0 +1,28 @@
+function selectOption(element) {
+    let parentGroup = element.parentNode;
+    let options = parentGroup.querySelectorAll('.filter-option');
+
+    options.forEach(opt => opt.classList.remove('active'));
+    element.classList.add('active');
+}
+
+// Thu gọn hoặc mở rộng bộ lọc
+function toggleFilter(header) {
+    let options = header.nextElementSibling;
+    let icon = header.querySelector('i');
+
+    if (options.classList.contains('visible')) {
+        // Thu gọn filter
+        options.classList.remove('visible');
+        setTimeout(() => {
+            options.style.maxHeight = "0px"; 
+        }, 10);
+        icon.classList.remove('rotate'); // Mũi tên quay xuống
+    } else {
+        // Mở filter
+        options.style.maxHeight = options.scrollHeight + "px"; // Đặt max-height đúng với nội dung
+        options.classList.add('visible');
+        icon.classList.add('rotate'); // Mũi tên quay lên
+    }
+}
+
