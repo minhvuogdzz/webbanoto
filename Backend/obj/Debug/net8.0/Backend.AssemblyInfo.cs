1c1d08a8e5399e438a83a28f064f14c05ad22ec1
minhvuogdzz <ougvn.it2@gmail.com>
2025-03-17 12:04:49 +0700

    update filesrc

--- Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c6747080e541c38ae26cd101bca663916532d58f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93be8b8a3de97a6266ae1ea43389326ac90095d5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
