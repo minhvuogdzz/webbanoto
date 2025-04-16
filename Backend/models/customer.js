5ecfaa6bc33f07f3d84f8218ee6075d9f07c7234
minhvuogdzz <ougvn.it2@gmail.com>
2025-04-16 12:58:33 +0700

    chẳng fix được cái gì

--- Backend/models/customer.js
+++ Backend/models/customer.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const CustomerSchema = new mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
-    phoneNumber: { type: String, required: true, unique: true}
+    phoneNumber: { type: String, required: true, unique: true }
 });
 
-module.exports = mongoose.model("Customer", CustomerSchema);
+module.exports = mongoose.model("customer", CustomerSchema);
